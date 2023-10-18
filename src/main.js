b9c22ba123f4d819b614cac995db62a7dcb0fbcc
Nick Wiley <nmw264@nau.edu>
2023-10-18 12:33:25 -0700

    Working basic server in nodejs

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 const http = require('http');
+const fs = require('fs').promises;
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-   res.statusCode = 200;
-   res.setHeader('Content-Type', 'text/plain');
-   res.end('Hello World!');
+   fs.readFile(__dirname + "/index.html").then(contents => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(contents);
+   }).catch(err => {
+      res.statusCode = 500;
+      res.end(err);
+      return;
+   });
 });
 
 server.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}/`);
 })
+
