06bac4fb6b4bcf6ecaab5106a10b56ad557b9e53
Nick Wiley <nmw264@nau.edu>
2023-10-20 17:09:09 -0700

    Updating index.html to run canvas.js

--- src/modules/canvas.js
+++ src/modules/canvas.js
@@ -1,7 +1,15 @@
 const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-if (canvas.getContext) {
-   const ctx = canvas.getContext("2d");
-} else {
-   print("Error getting context of canvas\n");
+function drawSquare(x, y, length, color) {
+   ctx.beginPath();
+   ctx.rect(x, y, length, length);
+   ctx.fillStyle = color;
+   ctx.fill();
+   ctx.closePath();
+}
+
+function draw() {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   drawSquare(20, 20, 50, "#FF0000");
 }
